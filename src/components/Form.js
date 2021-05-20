d97decb32d4173563261c3cbd2a156cb5c78eae0
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2021-05-20 19:21:34 -0400

    add id attribute to form element

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import Button from '@material-ui/core/Button';
 
 export function Form(props) {
   return (
-    <form className="form" onSubmit={(event) => props.onSubmit(event)}>
+    <form id="repsAndWeightForm" className="form" onSubmit={(event) => props.onSubmit(event)}>
       <label>
         Reps:
         <input id="repsInput" type="number" min="1" max="20" step="1"/>
